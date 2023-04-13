247ba5a34a4a8331e3f066b46477b5af5ce9ae25
Igor Rocha <igor_roc@hotmail.com.br>
2023-04-13 17:15:15 -0300

    feat: Profile Page

--- src/pages/app/Perfil/index.js
+++ src/pages/app/Perfil/index.js
@@ -1,12 +1,27 @@
-import React from "react"
+import React, { useContext } from "react"
 import { Text } from "react-native"
 import { View } from "react-native"
-import { Container } from "./styles"
+import { Container, SignOutButton } from "./styles"
+import { AuthContext } from "../../../contexts/auth"
+import { Link } from "@react-navigation/native"
 
 export default function Perfil() {
+	const { user } = useContext(AuthContext)
+
+	function handleSignOut() {
+		console.log("Sair")
+	}
+    
 	return (
 		<Container>
-			<Text>Perfil</Text>
+			<Text>Bem vindo(a) de volta</Text>
+			<Text>{user.name}</Text>
+			<Link to="/Home">
+				<Text>Home</Text>
+			</Link>
+			<SignOutButton onClick={handleSignOut}>
+				<Text>Sair</Text>
+			</SignOutButton>
 		</Container>
 	)
 }
--- src/pages/app/Perfil/styles.js
+++ src/pages/app/Perfil/styles.js
@@ -5,3 +5,14 @@ export const Container = styled.View`
 	align-items: center;
 	justify-content: center;
 `
+
+export const SignOutButton = styled.TouchableOpacity`
+	width: 90%;
+	margin-top: 20px;
+	padding: 10px;
+	background: transparent;
+	border: 1px solid #f73e3e;
+	border-radius: 7px;
+	justify-content: center;
+	align-items: center;
+`
